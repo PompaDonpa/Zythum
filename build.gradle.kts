b9c3f8677ce1c67db1b92a6c46ed46b4405b62be
Daniel Naranjo <mr.daniel.naranjo@protonmail.com>
2022-08-23 19:30:52 -0400

    Setup gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath (Build.androidBuildTools)
+        classpath (Build.hiltAndroidGradlePlugin)
+        classpath (Build.kotlinGradlePlugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id ("com.android.application") version "7.2.2" apply false
+    id ("com.android.library") version "7.2.2" apply false
+    kotlin ("android") version "1.6.10" apply false
+    kotlin ("jvm") version "1.6.10" apply false
+}
+
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,12 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
+
+    const val openBreweryData = ":openbrewery:openbrewery_data"
+    const val openBreweryDomain = ":openbrewery:openbrewery_domain"
+    const val openBreweryPresentation = ":openbrewery:openbrewery_presentation"
+}
